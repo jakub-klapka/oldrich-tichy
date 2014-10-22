4f1fb84ed7a398fc140dd5ebea977cadafb67cd1
Jakub Klapka <klapka@lumiart.cz>
2014-10-22 13:27:43 +0200

    Add gallery

---
+++ html/source_js/gallery.js
@@ -0,0 +1,174 @@
+/* global jQuery */
+( function ( $ ) {
+
+    var Gallery = {
+
+        init: function( el ) {
+
+            this.gallery = el;
+            this.full_image_el = this.gallery.find('.gallery__full_image');
+            this.full_image_image = this.full_image_el.find( '.gallery__full_image__image' );
+            this.full_image_close = this.full_image_el.find( '.gallery__full_image__close' );
+            this.full_image_prev = this.full_image_el.find( '.gallery__full_image__prev' );
+            this.full_image_next = this.full_image_el.find( '.gallery__full_image__next' );
+            this.full_image_desc = this.full_image_el.find( '.gallery__full_image__description' );
+
+            this.cat_menu = this.gallery.find( '.gallery__menu' );
+            this.cat_menu_items = this.cat_menu.find( '.gallery__menu__item' );
+
+            this.images_wrap = this.gallery.find('.gallery__items');
+            this.gallery_items = this.images_wrap.find( '.gallery__items__item' );
+
+            this.full_open = false;
+            this.prev_image = false;
+            this.next_image = false;
+
+            this.bindEvents();
+        },
+
+        bindEvents: function() {
+
+            var self = this;
+            this.images_wrap.on( 'click', '.gallery__items__item', function(){
+                self.showFull( $( this ), true );
+            } );
+
+            this.full_image_close.on( 'click', $.proxy( this.closeFullImage, this ) );
+
+            this.cat_menu.on( 'click', 'button', function() {
+                self.filter( $( this ) );
+            } );
+
+            this.full_image_prev.on( 'click', function(){
+                self.showFull( self.prev_image );
+            } );
+
+            this.full_image_next.on( 'click', function(){
+                self.showFull( self.next_image );
+            } );
+
+
+
+        },
+
+        showFull: function( image, scroll ) {
+            scroll = scroll || false;
+
+            this.gallery_items.removeClass( 'is-active' );
+            image.addClass( 'is-active' );
+
+            this.full_image_image
+                .attr( 'src', image.data( 'full' ) )
+                .attr( 'width', image.data( 'width' ) )
+                .attr( 'height', image.data( 'height' ) );
+            this.full_image_desc.html( image.find( '.gallery__items__item__description_placeholder' ).html() );
+
+            if( !this.full_open ) {
+                this.full_image_el.velocity( 'slideDown', { duration: 500 } );
+            }
+
+            this.full_open = true;
+
+            if( scroll ) {
+                $( 'body, html' ).animate( { scrollTop: $( '#dilo' ).offset().top }, 500 );
+            }
+
+            this.getPrevNext( image );
+
+            if( this.prev_image !== false ) {
+                this.full_image_prev.show();
+            } else {
+                this.full_image_prev.hide();
+            }
+
+            if( this.next_image !== false ) {
+                this.full_image_next.show();
+            } else {
+                this.full_image_next.hide();
+            }
+
+
+        },
+
+        getPrevNext: function( current ) {
+
+            var visible_images = this.gallery_items.filter( ':visible' ),
+                total = visible_images.length,
+                current_index = ( visible_images.index( current ) ) + 1;
+
+            if( current_index === 1 && current_index === total ) {
+                this.prev_image = false;
+                this.next_image = false;
+                return;
+            }
+
+            if( current_index === 1 ) {
+                this.prev_image = false;
+                this.next_image = visible_images.eq( ( current_index - 1 ) + 1 );
+                return;
+            }
+
+            if( current_index === total ) {
+                this.prev_image = visible_images.eq( ( current_index - 1 ) - 1 );
+                this.next_image = false;
+                return;
+            }
+
+            this.next_image = visible_images.eq( ( current_index - 1 ) + 1 );
+            this.prev_image = visible_images.eq( ( current_index - 1 ) - 1 );
+
+        },
+
+        closeFullImage: function() {
+            this.full_image_el.velocity( 'slideUp', { duration: 500 } );
+            this.gallery_items.removeClass( 'is-active' );
+            this.full_open = false;
+        },
+
+        filter: function( button ) {
+            this.cat_menu_items.removeClass( 'active' );
+            button.parent().addClass( 'active' );
+
+            if( this.full_open ) {
+                this.closeFullImage();
+            }
+
+            var cat = button.data('cat' ),
+                new_items,
+                old_items;
+
+            if( cat === 'all' ) {
+                new_items = this.gallery_items.not( ':visible' );
+                old_items = $();
+            } else {
+                var all_new_items = this.gallery_items.filter( '[data-cat="' + cat + '"]' );
+                old_items = this.gallery_items.filter( ':visible' ).not( all_new_items );
+                new_items = all_new_items.not( ':visible' );
+            }
+
+            old_items.velocity( 'fadeOut', {
+                duration: 500,
+                display: 'inline-block',
+                complete: function( el ){
+                    $( el ).hide();
+                }
+            } );
+            new_items.velocity( 'fadeIn', {
+                duration: 500,
+                display: 'inline-block'
+            } );
+
+        }
+
+    };
+
+
+    $( function() {
+
+        $( '.gallery' ).each( function(){
+            Object.create( Gallery ).init( $( this ) );
+        } );
+
+    } );
+
+} )( jQuery );
--- html/source_js/show_more.js
+++ html/source_js/show_more.js
@@ -34,12 +34,38 @@
                 this.open = true;
                 this.el.attr( 'aria-hidden', 'false' );
                 this.button.addClass( 'is-open' );
+                this.loadImages();
             } else {
                 this.el.velocity( 'slideUp', { duration: 500 } );
                 this.open = false;
                 this.el.attr( 'aria-hidden', 'true' );
                 this.button.removeClass( 'is-open' );
             }
+
+        },
+
+        loadImages: function() {
+
+            var images = this.el.find( '[data-image_placeholder]' );
+
+            if( images.length === 0 ) {
+                return;
+            }
+
+            images.each( function () {
+                var placeholder = $( this );
+                var image = $( '<img/>' );
+                image.attr( 'src', placeholder.data('src') )
+                    .attr( 'alt', placeholder.data( 'alt' ) )
+                    .attr( 'width', placeholder.data( 'width' ) )
+                    .attr( 'height', placeholder.data( 'height' ) )
+                    .attr( 'class', placeholder.data( 'class' ) )
+                    .attr( 'title', placeholder.data( 'title' ) );
+
+                placeholder.before( image );
+                placeholder.remove();
+
+            } );
 
         }
 
